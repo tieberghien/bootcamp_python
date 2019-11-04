99919914d0fdfb570325d5439a528ede5d77621f
Estelle TIEBERGHIEN <etieberg@e1r7p3.42.fr>
2019-11-04 16:55:04 +0100

    ex04 (error handling in progress)

---
+++ d00/ex04/operations.py
@@ -0,0 +1,24 @@
+import sys
+
+if __name__ == '__main__':
+    try:
+        n1 = float(sys.argv[1])
+        n2 = float(sys.argv[2])
+    except ValueError:
+        print("InputError: only number")
+        print("Usage: python operations.py\nExample:\n  python operations.py 10 3")
+        exit()
+    except BaseException:
+        print("Usage: python operations.py\nExample:\n  python operations.py 10 3")
+        exit()
+    print("Sum: %d" % int(n1+n2))
+    print("Difference: %d" % int(n1-n2))
+    print("Product: %d" % int(n1*n2))
+    try:
+        print("Quotient: %f" % (n1/n2))
+    except ZeroDivisionError as e:
+        print("Quotient: ERROR (div by zero)")
+    try:
+        print("Remainder: %d" % int(n1%n2))
+    except ZeroDivisionError as e:
+        print("Remainder: ERROR (modulo by zero)")
