5ee9753899e6b5d3ae1f8db91c092759ea763aa5
Estelle TIEBERGHIEN <etieberg@e1r13p8.42.fr>
2019-11-06 16:38:42 +0100

    ex00 almost done

---
+++ d01/ex00/book.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, name, last_update, creation_date, recipe_list):
+        pass
+
+    def get_recipe_by_name(self, name):
+        pass
+    
+    def get_recipes_by_types(self, recipe_type):
+        pass
+
+    def add_recipe(self, recipe):
+        pass
