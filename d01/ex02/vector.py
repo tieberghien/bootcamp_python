dbd979da3cbf1840ab3bf15833e82e62f46ac1c9
Estelle TIEBERGHIEN <etieberg@e1r13p8.42.fr>
2019-11-06 17:01:07 +0100

    ex02 started

---
+++ d01/ex02/vector.py
@@ -0,0 +1,14 @@
+class Vector:
+    def __init__(self, values, length):
+        try:
+            self.values = values
+            assert isinstance(values, list)
+        except:
+            print("{} type provided for attribute 'values' instead of 'list'".format(type(values)))
+            exit()
+        try:
+            self.length = length
+            assert isinstance(length, int)
+        except:
+            print("{} type provided for attribute 'length' instead of 'int'".format(type(length)))
+            exit()
