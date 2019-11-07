1e703b7ffbb0726c827886e208e8dedc710d1d6f
Estelle TIEBERGHIEN <etieberg@e1r11p22.42.fr>
2019-11-07 22:25:20 +0100

    done for the day

---
+++ d03/ex04/AdvancedFilter.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from numpy.linalg import inv
+
+class AdvancedFilter:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    img = mpimg.imread('../ex01/42AI.png')
+    af = AdvancedFilter()
