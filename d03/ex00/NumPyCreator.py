0b758565ce268f344fca7fb4ba8ad14f9abd4673
Estelle TIEBERGHIEN <etieberg@e1r11p22.42.fr>
2019-11-07 15:55:48 +0100

    random in progress

--- d03/ex00/NumPyCreator.py
+++ d03/ex00/NumPyCreator.py
@@ -13,17 +13,21 @@ class NumPyCreator:
     def from_iterable(self, itr):
         return np.fromiter(itr, int)
     
-    def from_shape(self, shape, value):
-        pass
+    def from_shape(self, shape, value=0):
+        return np.zeros((shape))
     
     def random(self, shape):
-        pass
+        return np.random.rand((shape))
 
     def identity(self, n):
-        pass
+        return np.identity(n)
 
 if __name__ == '__main__':
     npc = NumPyCreator()
     print(npc.from_list([[1,2,3],[6,3,4]]))
     print(npc.from_tuple(("a", "b", "c")))
     print(npc.from_iterable(range(5)))
+    shape=(3,5)
+    print(npc.from_shape(shape))
+    print(npc.random(shape))
+    print(npc.identity(4))
