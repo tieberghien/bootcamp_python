cbb095f504b051d6a22be161d1d27a32384c41fd
Estelle TIEBERGHIEN <etieberg@e1r11p22.42.fr>
2019-11-07 21:50:05 +0100

    ex02 done

--- d03/ex02/ScrapBooker.py
+++ d03/ex02/ScrapBooker.py
@@ -7,21 +7,36 @@ class ScraperBooker:
         pass
 
     def crop(self, array, dimensions, position=(0,0)):
-        return array[position[0]:dimensions, position[1]:dimensions]
+        try:
+            assert dimensions[0] < array.shape[0] and dimensions[1] < array.shape[1]
+        except:
+            print("'dimensions' can't be larger than size of image")
+            exit()
+        return array[position[0]:dimensions[0], position[1]:dimensions[1]]
 
     def thin(self, array, n, axis):
-        pass
+        return np.delete(array, np.s_[::n], axis)
 
     def juxtapose(self, array, n, axis):
-        pass
+        if axis == 0:
+            return np.vstack([array]*n)
+        elif axis == 1:
+            return np.hstack([array]*n)
+        else
+            print("Only accepts 2d images")
+            exit()
 
     def mosaic(self, array, dimensions):
-        pass
+        array = np.concatenate([array]*dimensions[0], axis=0)
+        array = np.concatenate([array]*dimensions[1], axis=1)
+        return array
 
 if __name__ == '__main__':
     img = mpimg.imread('../ex01/42AI.png')
-    print(img.shape[0], img.shape[1])
     sb = ScraperBooker()
-    cropped = sb.crop(img, 100, (50,50))
-    plt.imshow(cropped)
+    cropped = sb.crop(img, (100, 150), (50,50))
+    thinned = sb.thin(img, 2, 0)
+    juxt = sb.juxtapose(img, 3, 0)
+    mosaic = sb.mosaic(img, (2,3))
+    plt.imshow(mosaic)
     plt.show()
